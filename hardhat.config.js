45ef4cbe920bc9d0d11306040197ab9d1f21a0fb
motseki <lbmotseki@gmail.com>
2023-01-05 17:42:34 +0200

    Inital

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/MBNuGzLbVSW8rtDPhoCcZvSREuvmvwRG",
+      accounts: [ "162c0039398a93693bd44ae99c38b3d8b4771c97056b8a1e381dd453f2b9254d" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
